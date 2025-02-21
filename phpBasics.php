badb6421c6ad055921726ae3ce9e36b40e4fd6c5
Boris Goummo <goummoboris@gmail.com>
2025-02-21 16:28:21 +0100

    Learned about PHP constants

--- phpBasics.php
+++ phpBasics.php
@@ -1,6 +1,15 @@
 <?php
 $username = "Goummo Bill";
 $address = "780 Toyota Street, Bonapriso, Douala";
+$message = "This is inside php";
 
-echo "Your username is : \"$username\" and your address is : \"$address\""
+echo "Your username is : \"$username\" and your address is : \"$address\"";
+
+// check the variable type in php
+echo var_dump(PHP_FLOAT_MAX);
+
+// php numeric values boundaries
+echo ("PHP_FLOAT_MAX PHP_FLOAT_MIN PHP_FLOAT_DIG PHP_FLOAT_EPSILON");
+
+
 ?>
