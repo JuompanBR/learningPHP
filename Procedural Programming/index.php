70190dc96e83f786062a4c925beb5661955f09c5
Boris Goummo <goummoboris@gmail.com>
2025-03-06 13:19:46 +0100

    Progress on Procedural programming and PSR-12 standard

--- Procedural Programming/index.php
+++ Procedural Programming/index.php
@@ -1,8 +1,17 @@
-<?=
+<?php
 /** @var string $name*/
 $name = "Juompan Goummo Bill";
 
 /** @var int $age */
 $age = 22;
+
 echo "My name is Goummo Bill Boris";
+
+function marks() {
+
+    $name = "Name Changed to something else";
+    echo "<br>My name is".$name;
+}
+
+echo marks().marks();
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
 
-require_once "./encapsulation.php";
-
-$person_1 = new Person("Goummo Bill Boris", "780 Toyota street, Bonapriso, Douala", 23);
-
-echo $person_1->greet();
+echo "What is your name ?";
